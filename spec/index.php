fbe302ab3abc8e1aa892a4a52f74a009270cce08
Manu Sporny <msporny@digitalbazaar.com>
2010-10-14 15:10:10 -0400

    Initial checkin of json-ld.org.

---
+++ spec/index.php
@@ -0,0 +1,53 @@
+<?php
+print """"
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
+ "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"> 
+<html version="XHTML+RDFa 1.0" xmlns="http://www.w3.org/1999/xhtml"
+   xmlns:xhv="http://www.w3.org/1999/xhtml/vocab#"
+   xmlns:xsd="http://www.w3.org/2001/XMLSchema#"
+   xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+   xmlns:dcterms="http://purl.org/dc/terms/"
+   xmlns:vcard="http://www.w3.org/2006/vcard/ns#"
+   xmlns:v="http://rdf.data-vocabulary.org/#"
+   > 
+   <head> 
+      <meta http-equiv="Content-Type" content="text/html;charset=utf-8" /> 
+      <title>JSON-LD - Specifications</title> 
+      <link href="site.css" rel="stylesheet" type="text/css" /> 
+      <link rel="shortcut icon" href="favicon.ico" /> 
+      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script> 
+   </head> 
+ 
+   <body> 
+      <div id="container"> 
+         <div id="header"> 
+            <div class="col"> 
+               <h1>JSON-LD - Specifications</h1>
+            </div> 
+         </div> 
+
+         <div id="content"> 
+            <div id="info"> 
+               <h1>Latest Specification</h1> 
+               <p></p>
+            </div>
+"""";
+
+
+print """"
+         </div>
+ 
+         <div id="footer"> 
+            <p id="copyright"> 
+               Website content released under a <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution Share-Alike license</a> except where an alternate is specified.
+            </p> 
+            <p id="legal"> 
+               Part of the <a href="http://payswarm.com/">payswarm.com</a> initiative.
+            </p>
+         </div> 
+      </div> 
+   </body> 
+</html>
+"""";
+?>
+
