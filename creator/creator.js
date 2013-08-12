6cd7c60e941797109f7cc6b46edec4e4e568119d
Vikash Agrawal <vikashagrawal1990@gmail.com>
2013-08-13 02:23:30 +0530

    Validations for person-form and contexts referenced to w3id.org/schema

--- creator/creator.js
+++ creator/creator.js
@@ -19,18 +19,108 @@
 	return o;
   };
 
-  $('#form-person .input-large').on('change', function(){
-	var extended_json = $.extend({},contexts.person,$('#form-person').serializeObject())
+  // Form Person
+  
+  // Name
+  $('#form-person .input-xlarge[name="name"]').on('keyup', function(e){
+	var extended_json = $.extend({},contexts,$(this).serializeObject());
 	$('#json-ld').val(js_beautify(JSON.stringify(extended_json),{'indent_size': 2}));
   });
 
-  $('#form-event .input-large').on('change', function(){
-	var extended_json = $.extend({},contexts.events,$('#form-event').serializeObject())
+  // Image
+  $('#form-person .input-xlarge[name="image"]').on('keyup', function(e){
+  	var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+ 	var regex = new RegExp(expression);
+ 	if($(this).val().match(regex)) {
+	  var extended_json = $.extend({},contexts,$('#form-person').serializeObject());
+	  $('#json-ld').val(js_beautify(JSON.stringify(extended_json),{'indent_size': 2}));
+	}
+  });
+
+  $('#form-person .input-xlarge[name="address"]').on('keyup', function(e){
+	var extended_json = $.extend({},contexts,$('#form-person').serializeObject());
 	$('#json-ld').val(js_beautify(JSON.stringify(extended_json),{'indent_size': 2}));
   });
 
-  $('#form-place .input-large').on('change', function(){
-	var extended_json = $.extend({},contexts.place,$('#form-place').serializeObject())
+  $('#form-person .input-xlarge[name="email"]').on('keyup', function(e){
+	var expression = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+ 	var regex = new RegExp(expression);
+ 	if($(this).val().match(regex)) {
+	  var extended_json = $.extend({},contexts,$('#form-person').serializeObject());
+	  $('#json-ld').val(js_beautify(JSON.stringify(extended_json),{'indent_size': 2}));
+	}
+  });
+
+  $('#form-person .input-xlarge[name="phone"]').on('keyup', function(e){
+	var expression = /^[0-9]+$/;
+ 	var regex = new RegExp(expression);
+ 	if($(this).val().match(regex)) {
+	  var extended_json = $.extend({},contexts,$('#form-person').serializeObject());
+	  $('#json-ld').val(js_beautify(JSON.stringify(extended_json),{'indent_size': 2}));
+	}
+  });
+
+  $('#form-person .input-xlarge[name="gender"]').on('keyup', function(e){
+	var expression = /^(?:male|Male|female|Female)$/;
+ 	var regex = new RegExp(expression);
+ 	if($(this).val().match(regex)) {
+	  var extended_json = $.extend({},contexts,$('#form-person').serializeObject());
+	  $('#json-ld').val(js_beautify(JSON.stringify(extended_json),{'indent_size': 2}));
+	}
+  });
+
+  $('#form-person .input-xlarge[name="alumniOf"]').on('keyup', function(e){
+	var extended_json = $.extend({},contexts,$('#form-person').serializeObject());
+	$('#json-ld').val(js_beautify(JSON.stringify(extended_json),{'indent_size': 2}));
+  });
+
+  $('#form-person .input-xlarge[name="nationality"]').on('keyup', function(e){
+	var extended_json = $.extend({},contexts,$('#form-person').serializeObject());
+	$('#json-ld').val(js_beautify(JSON.stringify(extended_json),{'indent_size': 2}));
+  });
+
+  $('#form-person .input-xlarge[name="worksFor"]').on('keyup', function(e){
+	var extended_json = $.extend({},contexts,$('#form-person').serializeObject());
+	$('#json-ld').val(js_beautify(JSON.stringify(extended_json),{'indent_size': 2}));
+  });
+
+  $('#form-person .input-xlarge[name="workLocation"]').on('keyup', function(e){
+	var extended_json = $.extend({},contexts,$('#form-person').serializeObject());
+	$('#json-ld').val(js_beautify(JSON.stringify(extended_json),{'indent_size': 2}));
+  });
+
+  $('#form-person .input-xlarge[name="birthDate"]').datepicker();
+
+  $('#form-person .input-xlarge[name="birthDate"]').on('change', function(){
+	var extended_json = $.extend({},contexts,$('#form-person').serializeObject());
+	$('#json-ld').val(js_beautify(JSON.stringify(extended_json),{'indent_size': 2}));
+  });
+
+  // Form Event
+
+  $('#form-event .input-xlarge').on('keydown', function(e){
+	var extended_json = $.extend({},contexts,$('#form-event').serializeObject());
+	$('#json-ld').val(js_beautify(JSON.stringify(extended_json),{'indent_size': 2}));
+  });
+
+  $('#form-event .input-xlarge[name="startDate"]').datepicker();
+
+  $('#form-event .input-xlarge[name="startDate"]').on('change', function(){
+	var extended_json = $.extend({},contexts,$('#form-event').serializeObject());
+	$('#json-ld').val(js_beautify(JSON.stringify(extended_json),{'indent_size': 2}));
+  });
+
+  $('#form-event .input-xlarge[name="endDate"]').datepicker();
+
+  $('#form-event .input-xlarge[name="endDate"]').on('change', function(){
+	var extended_json = $.extend({},contexts,$('#form-event').serializeObject());
+	$('#json-ld').val(js_beautify(JSON.stringify(extended_json),{'indent_size': 2}));
+  });
+
+  // Form Place
+  
+  $('#form-place .input-xlarge').on('keydown', function(e){
+	var extended_json = $.extend({},contexts,$('#form-place').serializeObject());
 	$('#json-ld').val(js_beautify(JSON.stringify(extended_json),{'indent_size': 2}));
   });
 
@@ -70,3 +160,5 @@
   });
 
 })();
+
+
