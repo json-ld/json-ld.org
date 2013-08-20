b6ae13ccfc2a03e559d1e5152d91640999e0765f
Vikash Agrawal <vikashagrawal1990@gmail.com>
2013-08-20 11:06:53 +0530

    Added date in JSON-LD

--- linkedIn/linkedIn.js
+++ linkedIn/linkedIn.js
@@ -3,6 +3,10 @@ var contexts = {
 };
 
 var user = {};
+var linkedInData = {};
+var schemaOrgLinkedInData;
+var count = 0;
+var i;
 
 $(function(){
 	// $('#advanced-tab').hide();
@@ -31,14 +35,28 @@ function getDetails() {
 	  "id", "firstName", "lastName", "pictureUrl", "publicProfileUrl","location","headline","summary","specialties","positions","emailAddress","interests","publications","patents","languages","skills","certifications","educations","courses","volunteer","following","dateOfBirth","memberUrlResources","phoneNumbers","twitterAccounts","connections","network"
 	])
 	.result(function(result) {		
-		user["name"] = result.values[0].firstName +' '+ result.values[0].lastName;
+		user["firstName"] = result.values[0].firstName;
 		user["givenName"] = result.values[0].firstName;
 		user["familyName"] = result.values[0].lastName;
 		user["image"] = result.values[0].pictureUrl;
 		user["url"] = result.values[0].publicProfileUrl;
 		user["homeLocation"] = result.values[0].location.name;
 		user["description"] = result.values[0].headline;
+
+		for (i in result.values[0].dateOfBirth) {
+		  if (result.values[0].dateOfBirth.hasOwnProperty(i)) {
+		    count++;
+		  }
+		}
+
+		if(count == 3) {
+		  user["birthDate"] = result.values[0].dateOfBirth.year+'-'+result.values[0].dateOfBirth.month+'-'+result.values[0].dateOfBirth.day;
+		}
+		count = 0;
+		
 		var extended_json = $.extend({},contexts,user);
+		// if (console != undefined) {console.log(result);}
+		// schemaOrgLinkedInData = jsonld.compact(linkedInData, contexts);
 		$('#pane1 textarea').val(js_beautify(JSON.stringify(result),{'indent_size': 2}));
 		$('#json-ld').val(js_beautify(JSON.stringify(extended_json),{'indent_size': 2}));
 	})
