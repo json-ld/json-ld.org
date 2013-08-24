73f2c63f0c37f04f46bda17da3f6acfe321b282a
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-08-24 14:39:48 -0700

    Create vocab_gen.rb to build vocab.jsonld and vocab.html from vocab.ttl.

---
+++ test-suite/vocab_gen.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# Generate vocab.jsonld and vocab.html from vocab.ttl and vocab_template.
+#
+# Generating vocab.jsonld is equivalent to running the following:
+#
+#    jsonld --compact --context vocab_context.jsonld --input-format ttl vocab.ttl  -o vocab.jsonld
+require 'linkeddata'
+require 'haml'
+
+File.open("vocab.jsonld", "w") do |f|
+  r = RDF::Repository.load("vocab.ttl")
+  JSON::LD::API.fromRDF(r) do |expanded|
+    JSON::LD::API.compact(expanded, File.open("vocab_context.jsonld")) do |compacted|
+      # Create vocab.jsonld
+      f.write(compacted.to_json(JSON::LD::JSON_STATE))
+
+      # Create vocab.html using vocab_template.haml and compacted vocabulary
+      template = File.read("vocab_template.haml")
+      
+      html = Haml::Engine.new(template, :format => :html5).render(self,
+        :ontology => compacted['@graph'].detect {|o| o['@id'] == "http://json-ld.org/test-suite/vocab#"},
+        :classes => compacted['@graph'].select {|o| o['@type'] == "rdfs:Class"}.sort_by {|o| o['rdfs:label']},
+        :properties => compacted['@graph'].select {|o| o['@type'] == "rdfs:Property"}.sort_by {|o| o['rdfs:label']}
+      )
+      File.open("vocab.html", "w") {|fh| fh.write html}
+    end
+  end
+end
