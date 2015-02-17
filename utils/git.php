88d3444140d9050452ca6a5622f2258ddfcf11bf
David I. Lehn <dlehn@digitalbazaar.com>
2015-02-17 17:39:50 -0500

    More verbose pull hook output.
    - Show last line and retval.
    - Remove trailing whitespace.

--- utils/git.php
+++ utils/git.php
@@ -12,17 +12,19 @@ if($tfile == FALSE)
 else if(flock($tfile, LOCK_EX | LOCK_NB))
 {
    $token = trim(fgets($tfile));
-   
+
    // check to make sure that the token value is correct
    if(array_key_exists('token', $_GET) and $token === $_GET['token'])
    {
       // perform a git pull
       chdir('..');
       $gitdir = getcwd() . "/.git";
-      system("git --git-dir $gitdir pull");
-      
-      echo 'git update successful';
-      
+      $last_line = system("git --git-dir $gitdir pull", $last_line);
+
+      echo 'git pull last line: ' . $last_line . "\n";
+      echo 'git pull retval: ' . $retval . "\n";
+      echo 'git pull complete';
+
       // Sleep for 5 seconds to throttle the update rate to 12 per minute
       sleep(5);
    }
@@ -31,11 +33,11 @@ else if(flock($tfile, LOCK_EX | LOCK_NB))
       echo 'ERROR: Invalid secret token provided. ' .
          'See the README for more information.';
    }
-   
+
    // Release the lock file
    flock($tfile, LOCK_UN); // release the lock
 }
-else 
+else
 {
    echo 'ERROR: An update is currently being performed, ' .
       'this request has been rejected.';
