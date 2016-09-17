68c36f1230930c14045a304680367ec11b1cfdb7
Gregg Kellogg <gregg@greggkellogg.net>
2016-09-16 17:18:33 -0700

    Update internalDFN references using `postProcess` instead of an internal ReSpec event.

--- spec/latest/common/common.js
+++ spec/latest/common/common.js
@@ -63,88 +63,87 @@ function restrictReferences(utils, content) {
         }
     });
 
-    var $container = $(".termlist",base) ;
+    var $container = $(".termlist", base) ;
     var containerID = $container.makeID("", "terms") ;
     termLists.push(containerID) ;
+    return (base.innerHTML);
+}
 
-    // add a handler to come in after all the definitions are resolved
-    //
-    // New logic: If the reference is within a 'dl' element of
-    // class 'termlist', and if the target of that reference is
-    // also within a 'dl' element of class 'termlist', then
-    // consider it an internal reference and ignore it.
 
-    respecEvents.sub('end', function(message) {
-        if (message == 'core/link-to-dfn') {
-            // all definitions are linked; find any internal references
-            $(".termlist a.internalDFN").each(function() {
-                var $r = $(this);
-                var id = $r.attr('href');
-                var idref = id.replace(/^#/,"") ;
-                if (termNames[idref]) {
-                    // this is a reference to another term
-                    // what is the idref of THIS term?
-                    var $def = $r.closest('dd') ;
-                    if ($def.length) {
-                        var $p = $def.prev('dt').find('dfn') ;
-                        var tid = $p.attr('id') ;
-                        if (tid) {
-                            if (termsReferencedByTerms[tid]) {
-                                termsReferencedByTerms[tid].push(idref);
-                            } else {
-                                termsReferencedByTerms[tid] = [] ;
-                                termsReferencedByTerms[tid].push(idref);
-                            }
-                        }
+// add a handler to come in after all the definitions are resolved
+//
+// New logic: If the reference is within a 'dl' element of
+// class 'termlist', and if the target of that reference is
+// also within a 'dl' element of class 'termlist', then
+// consider it an internal reference and ignore it.
+
+function internalizeTermListReferences() {
+    // all definitions are linked; find any internal references
+    $(".termlist a.internalDFN").each(function() {
+        var $r = $(this);
+        var id = $r.attr('href');
+        var idref = id.replace(/^#/,"") ;
+        if (termNames[idref]) {
+            // this is a reference to another term
+            // what is the idref of THIS term?
+            var $def = $r.closest('dd') ;
+            if ($def.length) {
+                var $p = $def.prev('dt').find('dfn') ;
+                var tid = $p.attr('id') ;
+                if (tid) {
+                    if (termsReferencedByTerms[tid]) {
+                        termsReferencedByTerms[tid].push(idref);
+                    } else {
+                        termsReferencedByTerms[tid] = [] ;
+                        termsReferencedByTerms[tid].push(idref);
                     }
                 }
-            }) ;
+            }
+        }
+    });
 
-            // clearRefs is recursive.  Walk down the tree of 
-            // references to ensure that all references are resolved.
-            var clearRefs = function(theTerm) {
-                if ( termsReferencedByTerms[theTerm] ) {
-                    $.each(termsReferencedByTerms[theTerm], function(i, item) {
-                        if (termNames[item]) {
-                            delete termNames[item];
-                            clearRefs(item);
-                        }
-                    });
-                };
-                // make sure this term doesn't get removed
-                if (termNames[theTerm]) {
-                    delete termNames[theTerm];
-                }
-            };
-           
-            // now termsReferencedByTerms has ALL terms that 
-            // reference other terms, and a list of the 
-            // terms that they reference
-            $("a.internalDFN").each(function () {
-                var $item = $(this) ;
-                var t = $item.attr('href');
-                var r = t.replace(/^#/,"") ;
-                // if the item is outside the term list
-                if ( ! $item.closest('dl.termlist').length ) {
-                    clearRefs(r);
+    // clearRefs is recursive.  Walk down the tree of 
+    // references to ensure that all references are resolved.
+    var clearRefs = function(theTerm) {
+        if ( termsReferencedByTerms[theTerm] ) {
+            $.each(termsReferencedByTerms[theTerm], function(i, item) {
+                if (termNames[item]) {
+                    delete termNames[item];
+                    clearRefs(item);
                 }
             });
+        };
+        // make sure this term doesn't get removed
+        if (termNames[theTerm]) {
+            delete termNames[theTerm];
+        }
+    };
+   
+    // now termsReferencedByTerms has ALL terms that 
+    // reference other terms, and a list of the 
+    // terms that they reference
+    $("a.internalDFN").each(function () {
+        var $item = $(this) ;
+        var t = $item.attr('href');
+        var r = t.replace(/^#/,"") ;
+        // if the item is outside the term list
+        if ( ! $item.closest('dl.termlist').length ) {
+            clearRefs(r);
+        }
+    });
 
-            // delete any terms that were not referenced.
-            Object.keys(termNames).forEach(function(term) {
-                var $p = $("#"+term) ;
-                if ($p) {
-                    var tList = $p.getDfnTitles();
-                    $p.parent().next().remove();
-                    $p.remove() ;
-                    tList.forEach(function( item ) {
-                        if (respecConfig.definitionMap[item]) {
-                            delete respecConfig.definitionMap[item];
-                        }
-                    });
+    // delete any terms that were not referenced.
+    Object.keys(termNames).forEach(function(term) {
+        var $p = $("#"+term) ;
+        if ($p) {
+            var tList = $p.getDfnTitles();
+            $p.parent().next().remove();
+            $p.remove() ;
+            tList.forEach(function( item ) {
+                if (respecConfig.definitionMap[item]) {
+                    delete respecConfig.definitionMap[item];
                 }
             });
         }
     });
-    return (base.innerHTML);
 }
