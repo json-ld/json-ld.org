39c509611893bc885fa1e0ae350a3782cc2d41be
Manu Sporny <msporny@digitalbazaar.com>
2017-05-22 13:42:19 -0400

    Remove unnecessary code in proxy script. Add warning.

--- playground/proxy.php
+++ playground/proxy.php
@@ -1,20 +1,10 @@
 <?php
 /*
-* Warning! Read and use at your own risk!
-*
-* This tiny proxy script is completely transparent and it passes
-* all requests and headers without any checking of any kind.
-* The same happens with JSON data. They are simply forwarded.
-*
-* This is just an easy and convenient solution for the AJAX
-* cross-domain request issue, during development.
-* No sanitization of input is made either, so use this only
-* if you are sure your requests are made correctly and
-* your urls are valid.
-*
-*/
+ * This is a really dangerous proxy script, only run it on machines that
+ * can't cause any damage if they were to be pwnd.
+ */
 $method = $_SERVER['REQUEST_METHOD'];
-if ($_GET && $_GET['url']) {
+if($_GET && $_GET['url']) {
   $headers = getallheaders();
   $headers_str = array();
   $url = $_GET['url'];
@@ -30,20 +20,13 @@ if ($_GET && $_GET['url']) {
   }
 
   $ch = curl_init($url);
-  curl_setopt($ch,CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_URL, $url);
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers_str );
+  curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers_str);
   $result = curl_exec($ch);
   curl_close($ch);
   echo $result;
-}
-else {
-  echo $method;
-  var_dump($_POST);
-  var_dump($_GET);
-  $data_str = file_get_contents('php://input');
-  echo $data_str;
 }
 
 ?>
