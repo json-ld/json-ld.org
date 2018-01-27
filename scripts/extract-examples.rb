0681310c4c8efc6bc542326130f00180d994a7c0
Gregg Kellogg <gregg@greggkellogg.net>
2018-01-26 17:18:23 -0800

    Added script to extract examples and do basic checking to look for duplicate example titles and basic syntactic validation.
    Integrates with Travis-CI to validate examples in specs on commit.

--- scripts/extract-examples.rb
+++ scripts/extract-examples.rb
@@ -17,138 +17,144 @@ opts.each do |opt, arg|
   end
 end
 
-example_number = 0
-examples = {}
-errors = []
-warnings = []
+num_errors = 0
 
-File.open(ARGV.first, "r") do |f|
-  doc = Nokogiri::HTML.parse(f.read)
-  doc.css(".example").each do |element|
-    warn = false
-    example_number += 1 if element.name == "pre"
+ARGV.each do |input|
+  $stdout.puts "\ninput: #{input}"
+  example_number = 0
+  examples = {}
+  errors = []
+  warnings = []
 
-    if element.attr('data-ignore') || element.name == "table"
-      $stdout.write "i".colorize(:yellow)
-      next
-    end
+  File.open(input, "r") do |f|
+    doc = Nokogiri::HTML.parse(f.read)
+    doc.css(".example").each do |element|
+      warn = false
+      example_number += 1 if element.name == "pre"
 
-    if (title = element.attr('title')).to_s.empty?
-      errors << "Example #{example_number} at line #{element.line} has no title"
-      $stdout.write "F".colorize(:red)
-      next
-    end
+      if element.attr('data-ignore') || element.name == "table"
+        $stdout.write "i".colorize(:yellow)
+        next
+      end
 
-    if examples.any? {|n, ex| ex[:title] == title}
-      warnings << "Example #{example_number} at line #{element.line} uses duplicate title: #{title}"
-      warn = true
-    end
-
-    content = element.inner_html.
-      sub(/^\s*<!--\s*/m, '').
-      sub(/s*-->\s*$/m, '').
-      gsub('****', '').
-      gsub(/####([^#]*)####/, '')
-
-    ext = case element.attr('data-content-type')
-    when nil, '' then "json"
-    when 'application/n-quads', 'nq' then 'nq'
-    when 'text/html', 'html' then 'html'
-    when 'text/turtle', 'ttl' then 'ttl'
-    else 'txt'
-    end
-
-    # Perform example syntactic validation based on extension
-    case ext
-    when 'json'
-      begin
-        ::JSON.parse(content)
-      rescue JSON::ParserError => e
-        errors << "Example #{example_number} at line #{element.line} parse error: #{e.message}"
+      if (title = element.attr('title')).to_s.empty?
+        errors << "Example #{example_number} at line #{element.line} has no title"
         $stdout.write "F".colorize(:red)
         next
       end
-    when 'html'
-      begin
-        doc = Nokogiri::HTML.parse(content) {|c| c.strict}
-        doc.errors.each do |e|
-          errors << "Example #{example_number} at line #{element.line} parse error: #{e}"
-        end
-        unless doc.errors.empty?
+
+      if examples.any? {|n, ex| ex[:title] == title}
+        warnings << "Example #{example_number} at line #{element.line} uses duplicate title: #{title}"
+        warn = true
+      end
+
+      content = element.inner_html.
+        sub(/^\s*<!--\s*/m, '').
+        sub(/s*-->\s*$/m, '').
+        gsub('****', '').
+        gsub(/####([^#]*)####/, '')
+
+      ext = case element.attr('data-content-type')
+      when nil, '' then "json"
+      when 'application/n-quads', 'nq' then 'nq'
+      when 'text/html', 'html' then 'html'
+      when 'text/turtle', 'ttl' then 'ttl'
+      else 'txt'
+      end
+
+      # Perform example syntactic validation based on extension
+      case ext
+      when 'json'
+        begin
+          ::JSON.parse(content)
+        rescue JSON::ParserError => e
+          errors << "Example #{example_number} at line #{element.line} parse error: #{e.message}"
           $stdout.write "F".colorize(:red)
           next
         end
-      rescue Nokogiri::XML::SyntaxError => e
-        errors << "Example #{example_number} at line #{element.line} parse error: #{e.message}"
+      when 'html'
+        begin
+          doc = Nokogiri::HTML.parse(content) {|c| c.strict}
+          doc.errors.each do |e|
+            errors << "Example #{example_number} at line #{element.line} parse error: #{e}"
+          end
+          unless doc.errors.empty?
+            $stdout.write "F".colorize(:red)
+            next
+          end
+        rescue Nokogiri::XML::SyntaxError => e
+          errors << "Example #{example_number} at line #{element.line} parse error: #{e.message}"
+          $stdout.write "F".colorize(:red)
+          next
+        end
+      when 'ttl'
+        begin
+          reader_errors = []
+          RDF::Turtle::Reader.new(content, logger: reader_errors) {|r| r.validate!}
+        rescue
+          reader_errors.each do |e|
+            errors << "Example #{example_number} at line #{element.line} parse error: #{e}"
+          end
+          $stdout.write "F".colorize(:red)
+          next
+        end
+      when 'nq'
+        begin
+          reader_errors = []
+          RDF::NQuads::Reader.new(content, logger: reader_errors) {|r| r.validate!}
+        rescue
+          reader_errors.each do |e|
+            errors << "Example #{example_number} at line #{element.line} parse error: #{e}"
+          end
+          $stdout.write "F".colorize(:red)
+          next
+        end
+      end
+
+      case element.name
+      when "pre"
+        fn = "example-#{"%03d" % example_number}-#{title.gsub(/[^\w]+/, '-')}.#{ext}"
+        examples[example_number] = {
+          title: title,
+          filename: fn,
+          content: content
+        }
+        #puts "example #{example_number}: #{content}"
+      when "script"
+        # Validate the previous example appropriately
+      else
+        errors << "Example #{example_number} at line #{element.line} has unknown element type #{element.name}"
         $stdout.write "F".colorize(:red)
         next
       end
-    when 'ttl'
-      begin
-        reader_errors = []
-        RDF::Turtle::Reader.new(content, logger: reader_errors) {|r| r.validate!}
-      rescue
-        reader_errors.each do |e|
-          errors << "Example #{example_number} at line #{element.line} parse error: #{e}"
-        end
-        $stdout.write "F".colorize(:red)
-        next
-      end
-    when 'nq'
-      begin
-        reader_errors = []
-        RDF::NQuads::Reader.new(content, logger: reader_errors) {|r| r.validate!}
-      rescue
-        reader_errors.each do |e|
-          errors << "Example #{example_number} at line #{element.line} parse error: #{e}"
-        end
-        $stdout.write "F".colorize(:red)
-        next
+
+      if warn
+        $stdout.write "w".colorize(:yellow)
+      else
+        $stdout.write ".".colorize(:green)
       end
     end
+  end
 
-    case element.name
-    when "pre"
-      fn = "example-#{"%03d" % example_number}-#{title.gsub(/[^\w]+/, '-')}.#{ext}"
-      examples[example_number] = {
-        title: title,
-        filename: fn,
-        content: content
-      }
-      #puts "example #{example_number}: #{content}"
-    when "script"
-      # Validate the previous example appropriately
-    else
-      errors << "Example #{example_number} at line #{element.line} has unknown element type #{element.name}"
-      $stdout.write "F".colorize(:red)
-      next
-    end
+  $stdout.puts "\nWarnings:" unless warnings.empty?
+  warnings.each {|e| $stdout.puts "  #{e}".colorize(:yellow)}
+  $stdout.puts "\nErrors:" unless errors.empty?
+  errors.each {|e| $stdout.puts "  #{e}".colorize(:red)}
+  num_errors += errors.length
 
-    if warn
-      $stdout.write "w".colorize(:yellow)
-    else
-      $stdout.write ".".colorize(:green)
+  if example_dir
+    # Make examples directory
+    FileUtils::mkdir_p(example_dir)
+    examples.each do |num, ex|
+      File.open(File.join(example_dir, ex[:filename]), 'w') {|f| f.write(ex[:content])}
     end
   end
 end
 
-$stdout.puts "\nWarnings:" unless warnings.empty?
-warnings.each {|e| $stdout.puts "  #{e}".colorize(:yellow)}
-$stdout.puts "\nErrors:" unless errors.empty?
-errors.each {|e| $stdout.puts "  #{e}".colorize(:red)}
-
-if errors.empty? && warnings.empty?
+if num_errors == 0
   $stdout.puts "\nok".colorize(:green)
-elsif !errors.empty?
+else
   exit(1)
-end
-
-if example_dir
-  # Make examples directory
-  FileUtils::mkdir_p(example_dir)
-  examples.each do |num, ex|
-    File.open(File.join(example_dir, ex[:filename]), 'w') {|f| f.write(ex[:content])}
-  end
 end
 
 exit(0)
