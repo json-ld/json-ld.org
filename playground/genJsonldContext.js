cf59056ed8f60b4c2c5c68093ee25516a4ca9f62
Eric Prud'hommeaux <eric+github@w3.org>
2021-11-28 12:16:25 +0100

    + comments

--- playground/genJsonldContext.js
+++ playground/genJsonldContext.js
@@ -37,6 +37,9 @@ const NAMESPACES = {
   "w5": "http://hl7.org/fhir/w5#"
 };
 
+/**
+ * Recursive function to generate a content model for a FHIR Resource
+ */
 genJsonldContext = function (target, structureMap, datatypeMap, config) {
   console.log("genJsonldContext " + target);
   let map;
@@ -47,10 +50,13 @@ genJsonldContext = function (target, structureMap, datatypeMap, config) {
   } else if (target in datatypeMap) {
     map = datatypeMap;
   } else if (!(target in map)) throw new Error(`Key ${target} not found in ${Object.keys(map)}`);
+
   const resource = map[target];
-  if ("baseDefinition" in resource && !(resource.baseDefinition.startsWith(STRUCTURE_DEFN_ROOT))) throw new Error(`Don't know where to look for base structure ${resource.baseDefinition}`);
+  if ("baseDefinition" in resource && !(resource.baseDefinition.startsWith(STRUCTURE_DEFN_ROOT)))
+    throw new Error(`Don't know where to look for base structure ${resource.baseDefinition}`);
+
   const ret = "baseDefinition" in resource
-        ? genJsonldContext(
+        ? genJsonldContext( // Get content model from base type
           resource.baseDefinition.substr(STRUCTURE_DEFN_ROOT.length).toLowerCase(),
           structureMap,
           datatypeMap,
@@ -61,49 +67,66 @@ genJsonldContext = function (target, structureMap, datatypeMap, config) {
     globalThis.RET = ret;
   const backboneElements = {};
   let nowIn, resourceName;
+
+  // Walk differential elements
   resource.differential.element.forEach(elt => {
-    if (elt.id !== elt.path) throw new Error(`id !== path in ${target} ${map[target]}`);
+    if (elt.id !== elt.path) // test assumptions
+      throw new Error(`id !== path in ${target} ${map[target]}`);
+
+    // Early return for the first entry in a Resource's elements
     if (!("type" in elt)) { // 1st elt points to itself or something like that. Anyways, it doesn't have a type.
       backboneElements[''] = ret;
       return;
     }
+
+    // Calculate path components
     const path = elt.id.split('.');
     resourceName = path.shift();
-    if (resourceName.toLowerCase() !== target) throw new Error(`property id ${elt.id} does not start with target \"${target}\" in ${map[target]}`);
+    if (resourceName.toLowerCase() !== target)
+      throw new Error(`property id ${elt.id} does not start with target \"${target}\" in ${map[target]}`);
     let rawName = path.pop();
+
+    // Handle curried datatype names
     if (rawName.endsWith("[x]") ^ elt.type.length > 1)
       throw new Error(`Not sure whether ${target}.${elt.id} is a curried property or not: '${JSON.stringify(typeEntry)}'`);
     const [curried, name] = elt.type.length > 1
           ? [true, rawName.substr(0, rawName.length - "[x]".length)]
           : [false, rawName]; 
-    if (!Array.isArray(elt.type))
+
+    if (!Array.isArray(elt.type)) // test assuptions
       throw new Error(`unknown type list '${elt.id}' in ${JSON.stringify(map[target])}`);
+
+    // Walk the element's types
     elt.type.forEach((typeEntry, idx) => {
-      if (typeof typeEntry !== "object"
+      if (typeof typeEntry !== "object" // test assumptions
           || !("code" in typeEntry)
           || typeof typeEntry.code !== "string")
         throw new Error(`${idx}th type entry not recognized '${JSON.stringify(typeEntry)}' in ${JSON.stringify(map[target])}`);
+
+      // Calculate final element name.
       const typeCode = typeEntry.code;
       const curriedName = curried
             ? name + typeCode.substr(0, 1).toUpperCase() + typeCode.substr(1)
             : name;
       const left = path.join('.');
 
+      // Elements and BackboneElements imply a nested @context
       if (typeCode === "BackboneElement" || typeCode === "Element") {
         const backboneEltName = [...path, curriedName].join('.');
-        backboneElements[backboneEltName] = "baseDefinition" in resource // always true 'cause {,Backbone}Element have baseDefinitions.
-              ? genJsonldContext(
+        backboneElements[backboneEltName] = "baseDefinition" in resource  // apparently always true because
+              ? genJsonldContext(                                         // {,Backbone}Element have baseDefinitions.
                 resource.baseDefinition.substr(STRUCTURE_DEFN_ROOT.length).toLowerCase(),
                 structureMap,
                 datatypeMap,
                 config
               )
-              : JSON.parse(JSON.stringify(HEADER)); // never gets here
+              : JSON.parse(JSON.stringify(HEADER));                       // so it never gets here
         backboneElements[left][curriedName] = {
           '@id': `fhir:${elt.id}`,
           '@context': backboneElements[backboneEltName]
         };
       } else {
+        // Create a new property entry with the appropriate @context reference
         const trimmedTypeCode = typeCode.startsWith(FHIRPATH_ROOT)
               ? typeCode.substr(FHIRPATH_ROOT.length)
               : typeCode
@@ -114,5 +137,7 @@ genJsonldContext = function (target, structureMap, datatypeMap, config) {
       }
     });
   });
+
+  // Return the constructed @context.
   return ret;
 }
