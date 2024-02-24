f98f5d957a846e54da1c745fa29266eab70f4f50
David I. Lehn <dlehn@digitalbazaar.com>
2024-02-23 21:18:01 -0500

    Convert to Eleventy.
    - Copy and ignore files as needed.
    - Comment out old `prettify.css` usage.
    - Move `learn.html` to `learn/index.html` and update links. This could
      break external links, but is aligned with the pattern of other files
      and dirs.  Eleventy will put it in `learn/` by default either way
      without an override.
    - `specs/`, `primer/`, and `requirements/`:
      - Generate sub-dir listing data with JS.
      - Switch from PHP to LiquidJS.

---
+++ .eleventy.js
@@ -0,0 +1,52 @@
+const drafts = [
+  'CG-FINAL',
+  'CR',
+  'ED',
+  'FCGS',
+  'PR',
+  'REC',
+  'WD',
+  'latest'
+];
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('.htaccess');
+  eleventyConfig.addPassthroughCopy('LICENSE.md');
+  eleventyConfig.addPassthroughCopy('benchmarks/**/*.{jsonld,nq,md}');
+  eleventyConfig.addPassthroughCopy('contexts/**/*.{htaccess,html,jsonld}');
+  eleventyConfig.addPassthroughCopy('contexts/{event,person,place,recipe,remote-context}');
+  eleventyConfig.addPassthroughCopy('examples/**/*.{html,ttl,txt,json}');
+  eleventyConfig.addPassthroughCopy('favicon.ico');
+  eleventyConfig.addPassthroughCopy('fonts');
+  eleventyConfig.addPassthroughCopy('images/**/*.{htaccess,png,svg,xcf}');
+  eleventyConfig.addPassthroughCopy('ns/**/*.{html,jsonld}');
+  eleventyConfig.addPassthroughCopy('playground/**/*.{css,php,js}');
+  eleventyConfig.addPassthroughCopy('presentations');
+  eleventyConfig.addPassthroughCopy('schemas/**/*.json');
+  eleventyConfig.addPassthroughCopy('site.css');
+  eleventyConfig.addPassthroughCopy('spec/LICENSE.md');
+  for(const draft of drafts) {
+    eleventyConfig.addPassthroughCopy(`spec/${draft}`);
+  }
+  eleventyConfig.addPassthroughCopy('static');
+  eleventyConfig.addPassthroughCopy('test-suite');
+  eleventyConfig.addPassthroughCopy('utils');
+  eleventyConfig.ignores.add('CONTRIBUTING.md');
+  eleventyConfig.ignores.add('LICENSE.md');
+  eleventyConfig.ignores.add('README.rst');
+  eleventyConfig.ignores.add('benchmarks/README.md');
+  eleventyConfig.ignores.add('contexts/person.html');
+  eleventyConfig.ignores.add('examples');
+  eleventyConfig.ignores.add('images/README.md');
+  eleventyConfig.ignores.add('minutes/**/*');
+  eleventyConfig.ignores.add('ns/json-ld.html');
+  eleventyConfig.ignores.add('playground/dev/README.md');
+  eleventyConfig.ignores.add('presentations');
+  eleventyConfig.ignores.add('scripts');
+  eleventyConfig.ignores.add('spec/tools');
+  eleventyConfig.ignores.add('spec/LICENSE.md');
+  for(const draft of drafts) {
+    eleventyConfig.ignores.add(`spec/${draft}`);
+  }
+  eleventyConfig.ignores.add('test-suite');
+};
