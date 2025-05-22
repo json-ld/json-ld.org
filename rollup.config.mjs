53a74a3ecc38647d63706ecb95419fc00005f91f
Benjamin Young <byoung@digitalbazaar.com>
2025-05-22 10:05:13 -0400

    Add CBOR-LD tab. Very basic info to start.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,4 @@
+import commonjs from "@rollup/plugin-commonjs"
 import {nodeResolve} from "@rollup/plugin-node-resolve"
 export default {
   input: "./playground/next/editor.mjs",
@@ -5,5 +6,8 @@ export default {
     file: "./playground/next/editor.bundle.js",
     format: "iife"
   },
-  plugins: [nodeResolve()]
+  plugins: [
+    nodeResolve({browser: true}),
+    commonjs()
+  ]
 }
